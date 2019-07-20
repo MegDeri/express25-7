1223413972fdc910f58e908883db8eb3a31be053
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-07-20 22:09:55 +0300

    Final changes in pugs

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'500113384903-0gbi86d4e2cgmprn5r9fopkvgd2p1rgf.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'aEEPs4fksyYqllfcmzcYDRL2',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
